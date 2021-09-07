da2e05c55e562b08531829280fc80cc3cc804e42
Obelianko <bianka77@inbox.ru>
2021-09-07 23:28:53 +0300

    code update

--- src/test/java/ru/netology/web/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/web/CardDeliveryOrderTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -15,6 +15,15 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryOrderTest {
+    private String deliveryDate(int plusDays) {
+        LocalDate localDate = LocalDate.now();
+        LocalDate deliveryDate = localDate.plusDays(plusDays);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = deliveryDate.format(formatter);
+        return dateText;
+    }
+
+
     @BeforeEach
     public void setUp() {
         open("http://localhost:9999");
@@ -26,13 +35,11 @@ public class CardDeliveryOrderTest {
         form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
         form.$("[data-test-id=date] input").doubleClick();
         form.$("[data-test-id=date] input").sendKeys(Keys.DELETE);
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, 3);
-        form.$("[data-test-id=date] input").setValue(new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime()));
+        form.$("[data-test-id=date] input").setValue(deliveryDate(3));
         form.$("[data-test-id=name] input").setValue("Ольга Белянко");
         form.$("[data-test-id=phone] input").setValue("+79211111111");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button] .button__content").click();
-        $("[data-test-id=notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime())));
+        $("[data-test-id=notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + deliveryDate(3)));
     }
 }
