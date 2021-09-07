5457584c348f19784d29f8de69f1c54c15e35002
Obelianko <bianka77@inbox.ru>
2021-09-07 18:30:34 +0300

    card delivery test update

---
+++ src/test/java/ru/netology/web/CardDeliveryOrderTest.java
@@ -0,0 +1,38 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Calendar;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryOrderTest {
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    public void requestForDelivery() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.DELETE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 3);
+        form.$("[data-test-id=date] input").setValue(new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime()));
+        form.$("[data-test-id=name] input").setValue("Ольга Белянко");
+        form.$("[data-test-id=phone] input").setValue("+79211111111");
+        form.$("[data-test-id=agreement] .checkbox__box").click();
+        form.$("[role=button] .button__content").click();
+        $("[data-test-id=notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime())));
+    }
+}
